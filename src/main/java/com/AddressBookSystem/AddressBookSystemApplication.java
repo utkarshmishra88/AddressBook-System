d1d95c1eb05913103ac9189782469b2d8e37ece0
utkarshmishra88 <utkarshmishra8871@gmail.com>
2026-03-07 22:40:31 +0530

    [Utkarsh] : Added initial setup

---
+++ src/main/java/com/AddressBookSystem/AddressBookSystemApplication.java
@@ -0,0 +1,13 @@
+package com.AddressBookSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressBookSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressBookSystemApplication.class, args);
+	}
+
+}
